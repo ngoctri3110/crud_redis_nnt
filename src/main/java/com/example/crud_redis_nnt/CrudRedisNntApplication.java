ea9c30bd17c8699439415bb0197b948a4554010e
ngoctri3110 <62044877+ngoctri3110@users.noreply.github.com>
2021-11-05 08:05:27 +0700

    crud hash

---
+++ src/main/java/com/example/crud_redis_nnt/CrudRedisNntApplication.java
@@ -0,0 +1,27 @@
+package com.example.crud_redis_nnt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+
+@SpringBootApplication
+public class CrudRedisNntApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrudRedisNntApplication.class, args);
+    }
+    @Bean
+    LettuceConnectionFactory jedisConnectionFactory(){
+
+        return new LettuceConnectionFactory();
+    }
+
+    @Bean
+    RedisTemplate redisTemplate(){
+        RedisTemplate redisTemplate = new RedisTemplate();
+        redisTemplate.setConnectionFactory(jedisConnectionFactory());
+        return redisTemplate;
+    }
+}
---
+++ src/test/java/com/example/crud_redis_nnt/CrudRedisNntApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.crud_redis_nnt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudRedisNntApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
